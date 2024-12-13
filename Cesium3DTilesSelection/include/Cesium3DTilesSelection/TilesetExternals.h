458d09cf019d3ee91b7be80c824a486675e48ece
Julot <jdesreumaux@e-onsoftware.com>
2024-12-13 18:10:23 +0100

    [ITwin,glTFTuner,FastUVs] transmit the root translation to improve UVs

--- Cesium3DTilesSelection/include/Cesium3DTilesSelection/TilesetExternals.h
+++ Cesium3DTilesSelection/include/Cesium3DTilesSelection/TilesetExternals.h
@@ -40,7 +40,8 @@ public:
 	//! models needs to be re-tuned.
 	int currentVersion = 0;
 	virtual ~GltfTuner() = default;
-	virtual CesiumGltf::Model Tune(const CesiumGltf::Model& model, const glm::dmat4& tileTransform) = 0;
+  virtual CesiumGltf::Model Tune(const CesiumGltf::Model& model,
+    const glm::dmat4& tileTransform, const glm::dvec4& rootTranslation) = 0;
 	virtual void ParseTilesetJson(const rapidjson::Document& tilesetJson) = 0;
   //! The tuning may require some additional external data such as textures, typically in case of material
   //! customizations. In such case, we may need to use custom headers (holding an iTwin access token for
