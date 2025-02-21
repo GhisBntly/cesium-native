aab3838ac7e2993e041c5f0631fb1dd2b5a28954
Julot <jdesreumaux@e-onsoftware.com>
2025-02-21 11:41:16 +0100

    [iTwin,AdvViz,CreditSystem] added a way to prioritize credit texts
    - disabled by default (and only used for the user credit for now)

--- Cesium3DTilesSelection/src/TilesetContentManager.cpp
+++ Cesium3DTilesSelection/src/TilesetContentManager.cpp
@@ -602,7 +602,8 @@ TilesetContentManager::TilesetContentManager(
           (tilesetOptions.credit && externals.pCreditSystem)
               ? std::optional<Credit>(externals.pCreditSystem->createCredit(
                     tilesetOptions.credit.value(),
-                    tilesetOptions.showCreditsOnScreen))
+                    tilesetOptions.showCreditsOnScreen,
+                    tilesetOptions.creditPriority.value_or(-1)))
               : std::nullopt),
       _tilesetCredits{},
       _overlayCollection{std::move(overlayCollection)},
@@ -631,7 +632,8 @@ TilesetContentManager::TilesetContentManager(
           (tilesetOptions.credit && externals.pCreditSystem)
               ? std::optional<Credit>(externals.pCreditSystem->createCredit(
                     tilesetOptions.credit.value(),
-                    tilesetOptions.showCreditsOnScreen))
+                    tilesetOptions.showCreditsOnScreen,
+                    tilesetOptions.creditPriority.value_or(-1)))
               : std::nullopt),
       _tilesetCredits{},
       _overlayCollection{std::move(overlayCollection)},
@@ -777,7 +779,8 @@ TilesetContentManager::TilesetContentManager(
           (tilesetOptions.credit && externals.pCreditSystem)
               ? std::optional<Credit>(externals.pCreditSystem->createCredit(
                     tilesetOptions.credit.value(),
-                    tilesetOptions.showCreditsOnScreen))
+                    tilesetOptions.showCreditsOnScreen,
+                    tilesetOptions.creditPriority.value_or(-1)))
               : std::nullopt),
       _tilesetCredits{},
       _overlayCollection{std::move(overlayCollection)},
