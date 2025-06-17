ee02d7200b483c46ca6e407a517b9e16ec98888e
Ghislain Cottat <ghislain.cottat@bentley.com>
2025-06-17 13:35:54 +0200

    fix condition (consistency with isRenderable)

--- Cesium3DTilesSelection/src/Tile.cpp
+++ Cesium3DTilesSelection/src/Tile.cpp
@@ -297,7 +297,7 @@ bool Tile::needsWorkerThreadLoading(
               : std::nullopt;
       if (!latestVersion)
         latestVersion = renderContent->getModel().version;
-      if (!latestVersion || latestVersion < modelVersion)
+      if (!latestVersion || latestVersion != modelVersion)
         return true;
     }
   }
